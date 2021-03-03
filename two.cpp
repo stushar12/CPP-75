5e5af67d2376846a3ffc1d36701e671f762abd55
Tushar Saxena <stushar345@gmail.com>
2021-03-03 14:57:21 +0530

    Add files via upload

--- two.cpp
+++ two.cpp
@@ -1,23 +1,90 @@
-Node * abc(Node *root,Node * &prev)
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node 
 {
-    if(root==NULL)
-    return root;
-    Node * head=abc(root->left,prev);
-    if(prev==NULL)
-    head=root;
-    else
-    {
-        root->left=prev;
-        prev->right=root;
-    }
-    prev=root;
-    abc(root->right,prev);
-    return head;
-    
+	int data; 
+	Node* left;
+	Node* right;
+};
+
+Node* GetNewNode(int data) 
+{
+	Node* newNode = new Node();
+	newNode->data = data;
+	newNode->left = newNode->right = NULL;
+	return newNode;
 }
-Node * bToDLL(Node *root)
+
+Node* Insert(Node* root,int data) 
 {
-    Node *prev=NULL;
-    Node *a=abc(root,prev);
-    return a;
+	if(root == NULL)                                      // empty tree
+    {                                           
+		root = GetNewNode(data);
+	}
+	
+	else if(data <= root->data)                         // if data to be inserted is lesser, insert in left subtree. 
+    {
+		root->left = Insert(root->left,data);
+	}
+	
+	else                                                // else, insert in right subtree. 
+    {
+		root->right = Insert(root->right,data);
+	}
+	return root;
+}
+
+Node* BST_to_DLL(Node* root,Node* &prev)
+{
+	if(root==NULL)
+	return root;
+	Node* head=BST_to_DLL(root->left,prev);
+	if(prev==NULL)
+	head=root;
+	else
+	{
+	root->left=prev;
+	prev->right=root;
+	}
+	prev=root;
+	BST_to_DLL(root->right,prev);
+	return head;
+}
+
+
+void print_list(Node* root)
+{
+	Node* prev=NULL;
+	while(root!=NULL)
+	{
+		cout<<root->data<<" ";
+		prev=root;
+		root=root->right;
+	}
+	cout<<endl;
+	while(prev!=NULL)
+	{
+		cout<<prev->data<<" ";
+		prev=prev->left;
+	}
+	cout<<endl;
+}
+
+int main() 
+{
+	Node* root = NULL;  			// Creating an empty tree
+	Node* prev = NULL;
+	root = Insert(root,15);	
+	root = Insert(root,10);	
+	root = Insert(root,20);
+	root = Insert(root,8);
+	root = Insert(root,12);
+	root = Insert(root,17);
+	root = Insert(root,25);
+	
+	Node *head=BST_to_DLL(root,prev);
+	print_list(head);
+
+
 }
